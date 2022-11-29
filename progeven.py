52638d2a04605da63ee9faa7a5327a52bedd74ca
Noalacourarie <97045988+Noalacourarie@users.noreply.github.com>
2022-11-29 16:14:23 +0100

    Add files via upload
    ajout d'un canva

--- progeven.py
+++ progeven.py
@@ -7,25 +7,27 @@ def switch():
     label.config(image = photo) 
 
 def routeur():
-    label1.config(image = photo1) 
+    label1.config(image = photo1)
    
-
+dessin = Canvas(root, width=1600, height=920, bg='ivory')
+dessin.pack(side=TOP, padx=5, pady=5)
 button = Button(root, text='switch', command=switch)
-button.place(x='', y='100')
-button.grid(row=0,column=1)
+'''button.place(x='', y='100')'''
+button.pack(side=RIGHT, padx=5, pady=5)
 button1 = Button(root, text='routeur', command=routeur)
-button1.place(x='0', y='100')
-button1.grid(row=0,column=0)
+'''button1.place(x='0', y='100')'''
+button1.pack(side=LEFT,padx=5, pady=5)
 
 photo= PhotoImage(file='switch.png')
 label = Label(image = '')
-label.grid(row=5,column=5)
+label.pack()
 
 
 
 photo1= PhotoImage(file='routeur.png')
 label1 = Label(image = '')
-label1.grid(row=5,column=5)
+label1.pack()
+
 
 
 root.mainloop ()
